e0ae5ede3e3e98fd74ff935d153a3f6c8915e6b7
NeilClack <131825138+NeilClack@users.noreply.github.com>
2023-06-19 21:58:00 -0500

    Complete form and state management

--- src/components/form.js
+++ src/components/form.js
@@ -1,43 +1,79 @@
+import { useState } from "react";
+
 const UserForm = (props) => {
+  const [investmentData, setInvestmentData] = useState({
+    currentSavings: 0,
+    yearlyContribution: 0,
+    expectedReturn: 0.0,
+    duration: 0,
+  });
+
+  const handleSubmission = (event) => {
+    event.preventDefault();
+    console.log(investmentData);
+  };
+
+  const handleChange = (target, value) => {
+    // Remember to update state using a function when depending on the previous state.
+    setInvestmentData((prevData) => {
+      return {
+        ...prevData,
+        [target]: value,
+      };
+    });
+    console.log(investmentData);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmission}>
       <fieldset>
         <div className="leftContainer">
           <div className="fieldContainer">
-            <label for="currentSavings">Current Savings ($)</label>
+            <label htmlFor="currentSavings">Current Savings ($)</label>
             <input
               id="currentSavings"
               min="0"
-              step="100"
               type="number"
               placeholder="1000"
+              onChange={(event) => {
+                handleChange("currentSavings", event.target.value);
+              }}
+              value={investmentData.currentSavings}
             ></input>
           </div>
           <div className="fieldContainer">
-            <label for="yearlyContnribution">Yearly Contribution ($)</label>
+            <label htmlFor="yearlyContnribution">Yearly Contribution ($)</label>
             <input
               id="yearlyContribution"
               type="number"
               min="0"
-              step="100"
               placeholder="1000"
+              onChange={(event) => {
+                handleChange("yearlyContribution", event.target.value);
+              }}
+              value={investmentData.yearlyContribution}
             ></input>
           </div>
         </div>
         <div className="rightContainer">
           <div className="fieldContainer">
-            <label for="interestRate">Interest Rate</label>
+            <label htmlFor="expectedReturn">Interest Rate</label>
             <input
-              id="interestRate"
-              type="number"
+              id="expectedReturn"
+              type="float"
               min="0"
               max="100"
-              step=".25"
               placeholder="7.25"
+              onChange={(event) => {
+                handleChange("expectedReturn", event.target.value);
+              }}
+              value={investmentData.expectedReturn}
             ></input>
           </div>
           <div className="fieldContainer">
-            <label for="investmentDuration">Investment Duration (Years)</label>
+            <label htmlFor="investmentDuration">
+              Investment Duration (Years)
+            </label>
             <input
               id="investmentDuration"
               type="number"
@@ -45,10 +81,16 @@ const UserForm = (props) => {
               max="100"
               step="1"
               placeholder="10"
+              onChange={(event) => {
+                handleChange("duration", event.target.value);
+              }}
+              value={investmentData.duration}
             ></input>
           </div>
         </div>
-        <button type="submit" value="Submit">Submit</button>
+        <button type="submit" value="Submit">
+          Submit
+        </button>
       </fieldset>
     </form>
   );
